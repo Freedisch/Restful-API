002a40bfd430bb5120a20b25fd9fbef9191e310b
Freedisch <82499435+Freedisch@users.noreply.github.com>
2022-08-21 21:15:54 +0200

    too tired to comment

--- storefront/settings.py
+++ storefront/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
+    'rest_framework',
     'playground',
     'debug_toolbar',
     'store',
@@ -140,3 +142,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False
+}
